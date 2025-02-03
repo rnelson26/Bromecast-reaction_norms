701d217dce107489a7d23218a128e09e615c1005
rnelson26 <ranelson526@gmail.com>
2025-02-03 09:49:28 -0800

    updated with new information from Megan and Peter

--- Merge.R
+++ Merge.R
@@ -3,7 +3,7 @@
 ######## for bromecast reaction norm paper ########
 ######## R. Nelson, M. Vahsen, & P. Adler ######
 ########### code created on 1/28/25 #######
-############ last modified: 1/28/25 ########################
+############ last modified: 2/3/25 ########################
 
 ### outstanding questions ##########
 
@@ -21,37 +21,11 @@
 # MLV: Yes (?). I'm pretty sure that's correct, but let's double-check with
 # Peter.
 
-# 3. Is there a way to convert the phenology statuses of the common garden
-# plants into a simple yes vs no on whether they reproduced?
-
-# MLV: Yes. The column "first_flower" has the first date a flower was recorded.
-# If it is NA, then it didn't flower. So you could just make a binary column
-# based on that info.
-
-# 4. Was fecundity in the satellite sites measured differently than seed count
-# in the common garden?
-
-# MLV: Yes and no. Fecundity in the satellite sites was always counted by exact
-# number of seeds (because plants didn't get that big), where for most of the
-# common garden plants, seeds were subsampled, counted, and then total seed
-# count was estimated given the known inflorescence weights of the whole sample
-# and subsample.
-
-# 5. Is there a way to add lat/long & climate variables for the common garden
-# sites that match those used for the satellite sites?
-
-# MLV: We can add the lat/long, but I would hesitate to include those climate
-# variables. I don't think we will need them for the reaction norm analysis. The
-# lat/lon for the gardens are in the Bromecast-data Github >
-# rawdata/garden_info.csv
-
-# 6. Is it better to code NAs as character "not applicable" as I've currently
-# done or simply have them as NAs?
-
-# MLV: I would do "NA" preferably to be consistent. R treats "NA"s as different
-# than a character string that says "not applicable".
 
 ### load required packages ########
+rm(list = ls())
+
+
 library(tidyverse)
 
 ##### load data #########
@@ -76,7 +50,7 @@ colnames(sat)[colnames(sat) == "Year"] <- "year"
 sat$plantID <- 1:nrow(sat) ## assigns a unique number to each row (ind plant)
 
 #density
-colnames(sat)[colnames(sat) == "Treatment"] <- "density" #combine trt and density 
+colnames(sat)[colnames(sat) == "BRTE.neighbors"] <- "density" #combine BRTE neighbors and density 
 
 # transect
 cg$merged_block_plot <- paste(cg$block, cg$plot)
@@ -88,83 +62,109 @@ colnames(cg)[colnames(cg) == "live_harvest"] <- "Emerged"
 
 ## Reproduced
 
-## not sure if there is a way to link phenology statuses to whether or not they reproduced??
+cg <- cg %>%
+  mutate(Reproduced = case_when(
+    is.na(first_flower) ~ "No",
+    TRUE ~ "Yes"
+  ))
+
+# MLV: Yes. The column "first_flower" has the first date a flower was recorded.
+# If it is NA, then it didn't flower. So you could just make a binary column
+# based on that info.
 
 ## Fecundity
 colnames(cg)[colnames(cg) == "seed_count_total"] <- "Fecundity"
 
+#Fecundity in the satellite sites was always counted by exact
+# number of seeds (because plants didn't get that big), where for most of the
+# common garden plants, seeds were subsampled, counted, and then total seed
+# count was estimated given the known inflorescence weights of the whole sample
+# and subsample. so although we combined these two columns they were not identical in how they were measured.
+
 ## Biomass
-colnames(sat)[colnames(sat) == "Biomass"] <- "veg_mass"
-## need to check is the satellite biomass variable also includes infloresence mass 
+cg <- cg %>%
+  mutate(Biomass = veg_mass + inflor_mass)
+## check with Peter on this 
 
 ## add columns unique to common garden as blanks
-sat$albedo <- "Not_Applicable"
-sat$x <- "Not_Applicable"
-sat$y <- "Not_Applicable"
-sat$genotype <- "Not_Applicable"
-sat$block <- "Not_Applicable"
-sat$plot <- "Not_Applicable"
-sat$tillers <- "Not_Applicable"
-sat$note_standard_harvest <- "Not_Applicable"
-sat$inflor_mass <- "Not_Applicable"
-sat$first_flower <- "Not_Applicable"
-sat$v_phen <- "Not_Applicable"
-sat$last_phen_status <- "Not_Applicable"
-sat$note_standard_phen <- "Not_Applicable"
-sat$v_harvest <- "Not_Applicable"
-sat$note_standard_harvest <- "Not_Applicable"
+sat$albedo <- NA
+sat$x <- NA
+sat$y <- NA
+sat$genotype <- NA
+sat$block <- NA
+sat$plot <- NA
+sat$tillers <- NA
+sat$note_standard_harvest <- NA
+sat$inflor_mass <- NA
+sat$veg_mass <- NA
+sat$first_flower <- NA
+sat$v_phen <- NA
+sat$last_phen_status <- NA
+sat$note_standard_phen <- NA
+sat$v_harvest <- NA
+sat$note_standard_harvest <- NA
 
-cg$Distance <- "Not_Applicable"
-cg$Lat <- "Not_Applicable"
-cg$Lon <- "Not_Applicable"
-cg$prcp.Spr <- "Not_Applicable"
-cg$tmean.Spr <- "Not_Applicable"
-cg$swe_mean.Spr <- "Not_Applicable"
-cg$prcp.Sum <- "Not_Applicable"
-cg$tmean.Sum <- "Not_Applicable"
-cg$swe_mean.Sum <- "Not_Applicable"
-cg$prcp.Win <- "Not_Applicable"
-cg$tmean.Win <- "Not_Applicable"
-cg$swe_mean.Win <- "Not_Applicable"
-cg$prcp.Fall <- "Not_Applicable"
-cg$tmean.Fall <- "Not_Applicable"
-cg$swe_mean.Fall <- "Not_Applicable"
-cg$annual <- "Not_Applicable"
-cg$unknown <- "Not_Applicable"
-cg$perennial <- "Not_Applicable"
-cg$shrub <- "Not_Applicable"
-cg$groundcover <- "Not_Applicable"
-cg$biocrust <- "Not_Applicable"
-cg$fecundityflag <- "Not_Applicable"
-cg$notesFlag <- "Not_Applicable"
-cg$Reproduced <- "Not_Applicable"
+cg$Distance <- NA
+cg$Lat <- NA
+cg$Lon <- NA
+cg$prcp.Spr <- NA
+cg$tmean.Spr <- NA
+cg$swe_mean.Spr <- NA
+cg$prcp.Sum <- NA
+cg$tmean.Sum <- NA
+cg$swe_mean.Sum <- NA
+cg$prcp.Win <- NA
+cg$tmean.Win <- NA
+cg$swe_mean.Win <- NA
+cg$prcp.Fall <- NA
+cg$tmean.Fall <- NA
+cg$swe_mean.Fall <- NA
+cg$annual <- NA
+cg$unknown <- NA
+cg$perennial <- NA
+cg$shrub <- NA
+cg$groundcover <- NA
+cg$biocrust <- NA
+cg$fecundityflag <- NA
+cg$notesFlag <- NA
+cg$Treatment <- NA
 
 ### check dataframes to make sure they match
 colnames(cg)
 colnames(sat)
 
-listcg <- c("plantID", "site", "year", "density", "albedo", "block", "plot", "x", 
-           "y", "genotype", "first_flower", "v_phen", "last_phen_status", 
-           "note_standard_phen", "Emerged", "v_harvest", "tillers", "veg_mass", 
-           "inflor_mass", "Fecundity", "note_standard_harvest", "Type", "Transect", 
-           "Distance", "Lat", "Lon", "prcp.Spr", "tmean.Spr", "swe_mean.Spr", 
-           "prcp.Sum", "tmean.Sum", "swe_mean.Sum", "prcp.Win", "tmean.Win", 
-           "swe_mean.Win", "prcp.Fall", "tmean.Fall", "swe_mean.Fall", "annual", 
-           "unknown", "perennial", "shrub", "groundcover", "biocrust", "fecundityflag", 
-           "notesFlag", "Reproduced", "Type")
+# Assuming colnames1 and colnames2 represent your two lists
+cg_list <- c("plantID", "site", "year", "density", "albedo", "block", "plot", 
+               "x", "y", "genotype", "first_flower", "v_phen", "last_phen_status", 
+               "note_standard_phen", "Emerged", "v_harvest", "tillers", "veg_mass", 
+               "inflor_mass", "Fecundity", "note_standard_harvest", "Type", "Transect", 
+               "Reproduced", "Biomass", "Distance", "Lat", "Lon", "prcp.Spr", 
+               "tmean.Spr", "swe_mean.Spr", "prcp.Sum", "tmean.Sum", "swe_mean.Sum", 
+               "prcp.Win", "tmean.Win", "swe_mean.Win", "prcp.Fall", "tmean.Fall", 
+               "swe_mean.Fall", "annual", "unknown", "perennial", "shrub", 
+               "groundcover", "biocrust", "fecundityflag", "notesFlag", "Treatment")
 
-listsat <- c("site", "year", "density", "Transect", "Distance", "Emerged", "Reproduced", 
-           "Fecundity", "veg_mass", "fecundityflag", "notesFlag", "Lat", "Lon", 
-           "prcp.Spr", "tmean.Spr", "swe_mean.Spr", "prcp.Sum", "tmean.Sum", "swe_mean.Sum", 
-           "prcp.Win", "tmean.Win", "swe_mean.Win", "prcp.Fall", "tmean.Fall", "swe_mean.Fall", 
-           "annual", "unknown", "perennial", "shrub", "groundcover", "biocrust", "Type", 
-           "plantID", "albedo", "x", "y", "genotype", "block", "plot", "tillers", 
-           "note_standard_harvest", "inflor_mass", "first_flower", "v_phen", "last_phen_status", 
-           "note_standard_phen", "v_harvest")
+sat_list <- c("site", "year", "Treatment", "Transect", "Distance", "Emerged", 
+               "Reproduced", "density", "Fecundity", "Biomass", "fecundityflag", 
+               "notesFlag", "Lat", "Lon", "prcp.Spr", "tmean.Spr", "swe_mean.Spr", 
+               "prcp.Sum", "tmean.Sum", "swe_mean.Sum", "prcp.Win", "tmean.Win", 
+               "swe_mean.Win", "prcp.Fall", "tmean.Fall", "swe_mean.Fall", "annual", 
+               "unknown", "perennial", "shrub", "groundcover", "biocrust", "Type", 
+               "plantID", "albedo", "x", "y", "genotype", "block", "plot", 
+               "tillers", "note_standard_harvest", "inflor_mass", "veg_mass", 
+               "first_flower", "v_phen", "last_phen_status", "note_standard_phen", "v_harvest")
 
+# Check if they contain the same elements
+identical <- setequal(cg_list, sat_list)
+print(identical) 
 
-# Check if the lists are identical
-setequal(listcg, listsat)
+# Find differences
+diff1 <- setdiff(cg_list, sat_list)
+diff2 <- setdiff(cg_list, sat_list)
+
+print(diff1) # In colnames1 but not in colnames2
+print(diff2) # In colnames2 but not in colnames1
+
 
 #### merge the two dfs############
 
