7907a2df16aa8da14b25c6af3bd4f0b2ac5fd250
rnelson26 <ranelson526@gmail.com>
2025-02-11 09:04:33 -0800

    smapr troubleshooting

--- SMAP.R
+++ SMAP.R
@@ -3,12 +3,23 @@
 ######## for bromecast reaction norm paper ########
 ######## R. Nelson, M. Vahsen, & P. Adler ######
 ########### code created on 2/6/25 #######
-############ last modified: 2/6/25 ########################
+############ last modified: 2/11/25 ########################
+#https://github.com/ropensci/smapr/issues/81
 
 #### load packages #####
-library(smapr)
 library(terra)
+library(httr)
+library(jsonlite)
+library(rvest)
+library(smapr)
 
+# Install rvest 0.3.2
+devtools::install_version("rvest", version = "0.3.2")
+#https://github.com/ropensci/smapr/issues/33
+# doesn't fix issue
+
+packageVersion("rvest")
+available_data <- find_smap(id = "SPL3SMP", date = "2017-01-01", version = 4)
 #learn more here:
 #https://docs.ropensci.org/smapr/
   
@@ -20,7 +31,15 @@ library(terra)
 ## still waiting for NASA to approve the account for data access...
 
 ##### get the relevant SMAP dataset ####
-available_data <- find_smap(id = "SPL3SMAP", date = "2015-05-25", version = 3)
+  # Define timeframe of interest 
+  start_date <- as.Date("2021-01-01")
+  end_date <- as.Date("2024-12-31")
+  # Generate a sequence of dates
+  date_sequence <- seq(start_date, end_date, by = "day")
+  # Query SMAP data
+  available_data <- find_smap(id = "SPL3SMA", dates = date_sequence, version = 3)
+  
+
 str(available_data)
 
 downloads <- download_smap(available_data)
