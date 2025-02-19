56ea47ee0abf810ba2bd5b2d9a1645e827b4c13f
rnelson26 <ranelson526@gmail.com>
2025-02-19 14:16:15 -0800

    start on code for processing files

--- SMAP.R
+++ SMAP.R
@@ -3,63 +3,82 @@
 ######## for bromecast reaction norm paper ########
 ######## R. Nelson, M. Vahsen, & P. Adler ######
 ########### code created on 2/6/25 #######
-############ last modified: 2/18/25 ########################
-#https://github.com/ropensci/smapr/issues/81
+############ last modified: 2/19/25 ########################
+
 
 #### load packages #####
-#library(terra)
-#library(httr)
-#library(jsonlite)
-#library(rvest)
-#library(smapr)
-
-# Install rvest 0.3.2
-#devtools::install_version("rvest", version = "0.3.2")
-#https://github.com/ropensci/smapr/issues/33
-# doesn't fix issue
-
-#packageVersion("rvest")
-#available_data <- find_smap(id = "SPL3SMP", date = "2017-01-01", version = 4)
-#learn more here:
-#https://docs.ropensci.org/smapr/
+library(terra)  # For raster processing
+library(rhdf5)  # For reading HDF5 files
   
 
-###### to use smap we need a NASA Earth data account which links to Rstudio #######
+##### Processing SMAP data ####
 
-## See the instructions in the BromeCast Google drive (in manuscripts -> reaction norm model folder) for how to do this. I am avoiding putting this info directly in our public github repo to comply with NASA's data security guidelines....  
+# Inspect a specific file to list datasets
+h5_file <- "/Users/Becca/SMAP_data/SMAP_L2_SM_SP_1AIWDV_20200323T142815_20200324T015116_118W37N_R17000_001.h5"
+h5_contents <- h5ls(h5_file)
 
-## still waiting for NASA to approve the account for data access...
-
-##### get the relevant SMAP dataset ####
-
-page <- rvest::read_html("https:/https://nsidc.org/data/spl2smap_s/versions/3")  # Replace with the actual URL
-print(page)
-
-start_date <- as.Date("2015-03-31")
-end_date <- as.Date("2015-04-02")
-date_sequence <- seq(start_date, end_date, by = 1)
-find_smap(id = "SPL4SMGP", dates = date_sequence, version = 4)
+# Print the names of all datasets in the HDF5 file
+print(h5_contents)
 
 
+# Set working directory where the SMAP data is stored
+smap_dir <- "/Users/Becca/SMAP_data/"  
 
+# List all HDF5 files in the directory
+h5_files <- list.files(smap_dir, pattern = "\\.h5$", full.names = TRUE)
 
-  # Define timeframe of interest 
-start_date <- as.Date("2015-03-31")
-end_date <- as.Date("2015-04-02")
-  # Generate a sequence of dates
-  date_sequence <- seq(start_date, end_date, by = "day")
-  # Query SMAP data
-  available_data <- find_smap(id = "SPL3SMA", dates = date_sequence, version = 3)
+# Function to process a single SMAP HDF5 file
+process_smap_h5 <- function(h5_file) {
+  # Open the HDF5 file and check available datasets
+  h5_contents <- h5ls(h5_file)
   
+  # Define the dataset path for soil moisture
+  dataset_path <- "/Soil_Moisture_Retrieval_Data_3km/soil_moisture"
+  
+  # Check if the dataset exists in this file
+  if (!dataset_path %in% h5_contents$name) {
+    message("Dataset not found in file: ", h5_file)
+    return(NULL)
+  }
+  
+  # Read soil moisture data
+  soil_moisture <- h5read(h5_file, dataset_path)
+  
+  # Read latitude and longitude (ensure paths exist)
+  lat_path <- "/Soil_Moisture_Retrieval_Data_AM/latitude"
+  lon_path <- "/Soil_Moisture_Retrieval_Data_AM/longitude"
+  
+  if (!(lat_path %in% h5_contents$name) || !(lon_path %in% h5_contents$name)) {
+    message("Latitude or longitude missing in file: ", h5_file)
+    return(NULL)
+  }
+  
+  lat <- h5read(h5_file, lat_path)
+  lon <- h5read(h5_file, lon_path)
+  
+  # Ensure the data dimensions match (SMAP data is usually in a grid)
+  if (!all(dim(soil_moisture) == dim(lat), dim(soil_moisture) == dim(lon))) {
+    message("Dimension mismatch in file: ", h5_file)
+    return(NULL)
+  }
+  
+  # Create raster
+  r <- rast(soil_moisture)
+  
+  # Set spatial extent (bounding box)
+  ext(r) <- ext(min(lon), max(lon), min(lat), max(lat))  
+  crs(r) <- "EPSG:4326"  # WGS84 projection
+  
+  # Save as GeoTIFF
+  output_tif <- file.path(smap_dir, paste0(tools::file_path_sans_ext(basename(h5_file)), ".tif"))
+  writeRaster(r, output_tif, format = "GTiff", overwrite = TRUE)
+  
+  message("Processed and saved: ", output_tif)
+  return(output_tif)
+}
 
-  print(available_data)
+# Loop through all files and process them
+tif_files <- lapply(h5_files, process_smap_h5)
 
-str(available_data)
-
-downloads <- download_smap(available_data)
-
-list_smap(downloads, all = FALSE)
-
-sm_raster <- extract_smap(downloads, "Soil_Moisture_Retrieval_Data/soil_moisture")
-plot(sm_raster, main = "Level 3 soil moisture: May 25, 2015")
-writeRaster(sm_raster, "sm_raster.tif")
+# Print summary
+message("Finished processing ", length(na.omit(tif_files)), " files.")
