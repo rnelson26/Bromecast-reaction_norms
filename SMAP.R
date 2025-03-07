f62185961d70cd7f9ea90d0d3e53cbb200dac9a0
rnelson26 <ranelson526@gmail.com>
2025-03-06 16:43:35 -0800

    added USDA soil data to calculate water potential

--- SMAP.R
+++ SMAP.R
@@ -4,10 +4,12 @@
 ######## R. Nelson, M. Vahsen, & P. Adler ######
 ########### code created on 2/6/25 #######
 ############ last modified: 3/3/25 ########################
+rm(list = ls())
 
 ### read in file 
 SMAP <- read.csv("/Users/Becca/Desktop/Adler Lab/Bromecast-reaction_norms/data/SMAP.csv", header = TRUE)
-
+site_list <- read.csv("/Users/Becca/Desktop/Adler Lab/Bromecast-reaction_norms/data/site_list.csv", header = TRUE)
+horizon_data <- read.csv("/Users/Becca/Desktop/Adler Lab/Bromecast-reaction_norms/soil_data.csv", header = TRUE)
 
 ### organize data #####
 library(dplyr)
@@ -58,36 +60,64 @@ SMAP <- SMAP %>%
 # Replace -9999 with NA in the entire dataframe
 SMAP[SMAP == -9999] <- NA
 
+library(dplyr)
+
+
+
+YearlySummary <- SMAP %>%
+  group_by(Category, ID, Latitude, Longitude, Year) %>%
+  summarize(
+    soil_moisture_AM = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture, na.rm = TRUE),
+    soil_moisture_PM = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_pm, na.rm = TRUE),
+    soil_moisture_total = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture, na.rm = TRUE) + 
+      mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_pm, na.rm = TRUE),
+    soil_moisture_AM_dca = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture_dca, na.rm = TRUE),
+    soil_moisture_PM_dca = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_dca_pm, na.rm = TRUE),
+    soil_moisture_total_dca = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture_dca, na.rm = TRUE) + 
+      mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_dca_pm, na.rm = TRUE)
+  ) %>%
+  ungroup()
+
+
 
 MonthlySummary = SMAP %>%
   group_by(Category, ID, Latitude, Longitude, Year, Month) %>%
   summarize(
-    soil_moisture_AM = sum(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture, na.rm = TRUE),
-    soil_moisture_PM = sum(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_pm, na.rm = TRUE),
-    soil_moisture_total = sum(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture, na.rm = TRUE) + 
-      sum(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_pm, na.rm = TRUE),
-    soil_moisture_AM_dca = sum(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture_dca, na.rm = TRUE),
-    soil_moisture_PM_dca = sum(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_dca_pm, na.rm = TRUE),
-    soil_moisture_total_dca = sum(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture_dca, na.rm = TRUE) + 
-      sum(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_dca_pm, na.rm = TRUE)
+    soil_moisture_AM = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture, na.rm = TRUE),
+    soil_moisture_PM = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_pm, na.rm = TRUE),
+    soil_moisture_total = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture, na.rm = TRUE) + 
+      mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_pm, na.rm = TRUE),
+    soil_moisture_AM_dca = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture_dca, na.rm = TRUE),
+    soil_moisture_PM_dca = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_dca_pm, na.rm = TRUE),
+    soil_moisture_total_dca = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture_dca, na.rm = TRUE) + 
+      mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_dca_pm, na.rm = TRUE)
   )
 
 
 DailySummary = SMAP %>%
   group_by(Category, ID, Latitude, Longitude, Year, Month, Julian_day) %>%
   summarize(
-    soil_moisture_AM = sum(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture, na.rm = TRUE),
-    soil_moisture_PM = sum(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_pm, na.rm = TRUE),
-    soil_moisture_total = sum(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture, na.rm = TRUE) + 
-      sum(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_pm, na.rm = TRUE),
-    soil_moisture_AM_dca = sum(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture_dca, na.rm = TRUE),
-    soil_moisture_PM_dca = sum(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_dca_pm, na.rm = TRUE),
-    soil_moisture_total_dca = sum(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture_dca, na.rm = TRUE) + 
-      sum(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_dca_pm, na.rm = TRUE)
+    soil_moisture_AM = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture, na.rm = TRUE),
+    soil_moisture_PM = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_pm, na.rm = TRUE),
+    soil_moisture_total = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture, na.rm = TRUE) + 
+      mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_pm, na.rm = TRUE),
+    soil_moisture_AM_dca = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture_dca, na.rm = TRUE),
+    soil_moisture_PM_dca = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_dca_pm, na.rm = TRUE),
+    soil_moisture_total_dca = mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_AM_soil_moisture_dca, na.rm = TRUE) + 
+      mean(SPL3SMP_E_006_Soil_Moisture_Retrieval_Data_PM_soil_moisture_dca_pm, na.rm = TRUE)
   )
 
 
 # Make individual plots for each site
+
+YearlySummary %>% 
+  ggplot(aes(x = Year, y = soil_moisture_PM_dca, color = ID)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~ID*Category) +
+  theme_bw(base_size = 16) +
+  theme(legend.position = "none")
+
 DailySummary %>% 
   filter(Year == 2022) %>% 
   ggplot(aes(x = Julian_day, y = soil_moisture_PM_dca, color = ID)) +
@@ -118,6 +148,15 @@ MonthlySummary %>%
 MonthlySummary %>% 
   filter(Year == 2021) %>% 
   ggplot(aes(x = Month, y = soil_moisture_AM_dca, color = ID)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~ID*Category) +
+  theme_bw(base_size = 16) +
+  theme(legend.position = "none")
+
+MonthlySummary %>% 
+  filter(Year == 2021) %>% 
+  ggplot(aes(x =soil_moisture_AM, y = soil_moisture_AM_dca, color = ID)) +
   geom_point() +
   geom_line() +
   facet_wrap(~ID*Category) +
@@ -157,6 +196,28 @@ library("fields")
 color_palette <- colorRampPalette(c("blue", "yellow", "red"))
 colors <- color_palette(100)  
 
+### by year
+#check why one point is floating off the map
+
+# Convert state map data
+state_map <- map_data("state")
+state_map_filtered <- state_map[state_map$long >= -128 & state_map$long <= -95 &
+                                  state_map$lat >= 30 & state_map$lat <= 52, ]
+
+
+YearlySummary$color <- colors[cut(YearlySummary$soil_moisture_PM, breaks = 100)]
+
+ggplot() +
+  geom_polygon(data=state_map_filtered, aes(x=long, y=lat, group=group), fill="gray100", color="black") +
+  geom_point(data=YearlySummary, aes(x=Longitude, y=Latitude, color=soil_moisture_PM)) +
+  scale_color_gradientn(colors=colors) +
+  coord_cartesian(xlim=c(-128,-95), ylim=c(30,52)) +  # Set zoom area
+  facet_wrap(~Year) +
+  labs(title="Soil Moisture PM by Year", color="Soil Moisture PM") +
+  theme_classic()
+
+#### by variable 
+
 MonthlySummary$color <- colors[cut(MonthlySummary$soil_moisture_PM, breaks = 100)]  
 par(mar=c(4,4,4,4))
 map("state",xlim=c(-128,-95),ylim=c(30,52))
@@ -187,19 +248,168 @@ points(MonthlySummary$Longitude, MonthlySummary$Latitude, col = MonthlySummary$c
 image.plot(legend.only = TRUE, zlim = range(MonthlySummary$soil_moisture_AM_dca), col = colors, legend.lab = "Soil Moisture AM DCA")
 
 
+###### Retreive soil texture #########
+# Install and load necessary packages
+#library(httr)
+library(soilDB)
+library(soiltexture)
+library(aqp)
+library(sf)
+
+
+
+# Ensure site_list has correct column names
+x <- site_list %>% 
+  rename(id = ID, lat = Latitude, lon = Longitude) %>% 
+  select(id, lat, lon)
+
+# Fetch SoilGrids data
+soil_data <- fetchSoilGrids(
+  x = x,
+  loc.names = c("id", "lat", "lon"),
+  depth_intervals = c("0-5", "5-15", "15-30", "30-60", "60-100", "100-200"),
+  variables = c("bdod", "cec", "cfvo", "clay", "nitrogen", "phh2o", "sand", "silt",
+                "soc", "ocd", "wv0010", "wv0033", "wv1500"),
+  grid = FALSE,  # Ensures point data retrieval instead of grid-based data
+  target_resolution = c(250, 250),  # Resolution in meters
+  summary_type = c("Q0.05", "Q0.5", "Q0.95", "mean"),  # Include multiple statistics
+  verbose = TRUE,  # Show progress messages
+  progress = TRUE  # Show download progress
+)
+
+# Check structure of the returned data
+str(soil_data)
+
+# Print a preview of the soil data
+head(soil_data)
+
+## extract info
+horizon_data <- horizons(soil_data)
+
+## explore info
+library(ggplot2)
+
+# Example: Assuming your data is in a dataframe called `soil_data`
+ggplot(horizon_data, aes(x = id, y = clayQ50)) +
+geom_boxplot() + 
+ theme_classic()
+  labs(title = "Median Clay Fraction (clayQ50) by Site",
+       x = "Site",
+       y = "Clay Fraction (Median)") 
+
+  ggplot(horizon_data, aes(x = id, y = claymean)) +
+    geom_boxplot() +
+    theme_classic()
+  labs(title = "Median Clay Fraction (clayQ50) by Site",
+       x = "Site",
+       y = "Clay Fraction (Median)") 
+  
+  ggplot(horizon_data, aes(x = id, y = siltQ50)) +
+    geom_boxplot() + 
+    theme_classic()
+  labs(title = "Median Clay Fraction (clayQ50) by Site",
+       x = "Site",
+       y = "Clay Fraction (Median)") 
+  
+  ggplot(horizon_data, aes(x = id, y = siltmean)) +
+    geom_boxplot() +
+    theme_classic()
+  labs(title = "Median Clay Fraction (clayQ50) by Site",
+       x = "Site",
+       y = "Clay Fraction (Median)") 
+  
+  ggplot(horizon_data, aes(x = id, y = sandQ50)) +
+    geom_boxplot() + 
+    theme_classic()
+  labs(title = "Median Clay Fraction (clayQ50) by Site",
+       x = "Site",
+       y = "Clay Fraction (Median)") 
+  
+  ggplot(horizon_data, aes(x = id, y = sandmean)) +
+    geom_boxplot() +
+    theme_classic()
+  labs(title = "Median Clay Fraction (clayQ50) by Site",
+       x = "Site",
+       y = "Clay Fraction (Median)") 
+  
+  #write.csv(horizon_data, "soil_data.csv", row.names = FALSE)
+  
+ 
 ######## Use a Van Genuchten Model to calculate water potential ######
-# Define soil parameters (example for sandy loam)
-theta_s <- 8.1 #estimate based on being slightly bigger than max observed value
-alpha <- 0.075   # Air entry parameter (1/cm)
-n <- 1.89        # Shape parameter
-m <- 1 - 1/n     # Derived parameter
-## can modify these more based on soil type, but started with middle of the road values 
-## would need more detailed info about soil types to do this in a way that gets realistic water potential values...
+#https://ncss-tech.github.io/AQP/soilDB/ROSETTA-API.html
+  
 
-MonthlySummary <- MonthlySummary %>% mutate(water_potential =  ((1/alpha) * (((soil_moisture_AM/theta_s)^(-1/m)) - 1)^(1/n)) * -0.00981 )
+  # call ROSETTA API 
+  vars <- c('claymean', 'siltmean', 'sandmean')
+  r <- ROSETTA(horizon_data, vars)  
 
-MonthlySummary %>% 
-  filter(Year == 2021) %>% 
+r$ID <- r$id  
+
+ 
+r_summary <- r %>%
+  select(ID, theta_r, claymean, siltmean, sandmean, alpha, npar, ksat, theta_s) %>% 
+  group_by(ID) %>% 
+  summarise(across(everything(), \(x) mean(x, na.rm = TRUE)))
+
+
+MonthlySummary_r <- left_join(MonthlySummary, r_summary, by= "ID")
+YearlySummary_r <- left_join(YearlySummary, r_summary, by= "ID")
+
+# Define a function to calculate water potential using Rosetta model
+# Define the function for calculating water potential
+calculate_water_potential <- function(claymean, siltmean, sandmean, theta_s, theta_r, alpha, soil_moisture, npar) {
+  # Use Rosetta API to calculate the water potential
+  water_potential <- (1 / alpha) * (((soil_moisture / theta_s) ^ (-1 / (1 - 1 / log10(npar)))) - 1) ^ (1 / log10(npar))
+  # Convert to pressure head in kPa (assuming SI units)
+  return(water_potential * -0.00981)
+}
+
+
+YearlySummary_r <- YearlySummary_r %>%
+  mutate(
+    water_potential = mapply(
+      calculate_water_potential,
+      clay = claymean,
+      silt = siltmean,
+      sand = sandmean,
+      theta_s = theta_s,
+      theta_r = theta_r,
+      alpha = alpha,
+      npar = npar,
+      soil_moisture = soil_moisture_AM
+    )
+  )
+
+MonthlySummary_r <- MonthlySummary_r %>%
+  mutate(
+    water_potential = mapply(
+      calculate_water_potential,
+      clay = claymean,
+      silt = siltmean,
+      sand = sandmean,
+      theta_s = theta_s,
+      theta_r = theta_r,
+      alpha = alpha,
+      npar = npar,
+      soil_moisture = soil_moisture_AM
+    )
+  )
+
+
+#theta_r : residual volumetric water content
+#theta_s : saturated volumetric water content
+#log10(alpha) : retention shape parameter ⁠[log10(1/cm)]⁠
+#log10(npar) : retention shape parameter
+#log10(ksat) : saturated hydraulic conductivity ⁠[log10(cm/d)]⁠
+
+YearlySummary_r %>% 
+  ggplot(aes(x = ID, y = water_potential, color = ID)) +
+  geom_boxplot() +
+  facet_wrap(~Category) +
+  theme_bw(base_size = 16) +
+  theme(legend.position = "none")
+
+MonthlySummary_r %>% 
   ggplot(aes(x = Month, y = water_potential, color = ID)) +
   geom_point() +
   geom_line() +
@@ -214,12 +424,29 @@ MonthlySummary <- MonthlySummary %>%
 color_palette <- colorRampPalette(c("blue", "yellow", "red"))
 colors <- color_palette(100)  
 
-MonthlySummary$color <- colors[cut(MonthlySummary$water_potential, breaks = 100)]  
+YearlySummary_r$color <- colors[cut(YearlySummary_r$water_potential, breaks = 100)]  
 par(mar=c(4,4,4,4))
 map("state",xlim=c(-128,-95),ylim=c(30,52))
 title("Water Potential")
-points(MonthlySummary$Longitude, MonthlySummary$Latitude, col = MonthlySummary$color, pch = 19, cex = 1.5)
-image.plot(legend.only = TRUE, zlim = range(MonthlySummary$water_potential), col = colors, legend.lab = "Water Potential")
+points(YearlySummary_r$Longitude, YearlySummary_r$Latitude, col = YearlySummary_r$color, pch = 19, cex = 1.5)
+image.plot(legend.only = TRUE, zlim = range(YearlySummary_r$water_potential), col = colors, legend.lab = "Water Potential")
+
+state_map <- map_data("state")
+state_map_filtered <- state_map[state_map$long >= -128 & state_map$long <= -95 &
+                                  state_map$lat >= 30 & state_map$lat <= 52, ]
+
+
+YearlySummary_r$color <- colors[cut(YearlySummary_r$water_potential, breaks = 100)]
+
+ggplot() +
+  geom_polygon(data=state_map_filtered, aes(x=long, y=lat, group=group), fill="gray100", color="black") +
+  geom_point(data=YearlySummary_r, aes(x=Longitude, y=Latitude, color=water_potential)) +
+  scale_color_gradientn(colors=colors) +
+  coord_cartesian(xlim=c(-128,-95), ylim=c(30,52)) +  # Set zoom area
+  facet_wrap(~Year) +
+  labs(title="Water Potential by Year", color="Water Potential") +
+  theme_classic()
+
 
 ###### Merge with combined dataframe #####
 
